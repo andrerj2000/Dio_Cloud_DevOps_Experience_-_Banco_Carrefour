790aff9c8b71b844b4a7649e825df8fa3490240a
andrerj2000 <andrerj2000@gmail.com>
2023-01-20 16:35:47 -0300

    Organização das pastas!

--- Container-Web/worker.sh
+++
@@ -1,1 +0,0 @@
-    docker swarm join --token SWMTKN-1-1ch33f22y46p4vbt7w25qveloz3l9d7cjc7pl3262lho96g920-9m22jq6u00bwh8aoqda0ilqtg 10.0.0.210:2377
---
+++ Criando_um_Deploy_de_uma_Aplicação/worker.sh
@@ -0,0 +1,1 @@
+    docker swarm join --token SWMTKN-1-5s8f3zuu9ioke1ub9rsothpn3fte28u6gs7np5xlwyk2li8gn7-a2th7pzw8obc73130dzgn0ht6 10.0.0.210:2377
